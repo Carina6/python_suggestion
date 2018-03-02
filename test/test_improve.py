38e7f5186f9c702230a259232d6089bbc02126cb
liuhonglian <liuhonglian@xiaomi.com>
2018-03-02 18:32:19 +0800

    add metaclass implement

--- test/test_improve.py
+++ test/test_improve.py
@@ -425,3 +425,69 @@ def test_metaclass():
     f = Foo()
     print(f.BAR)
 
+
+# 63：熟悉 Python 对象协议
+def test_protocol():
+    class A(object):
+        # 字符串格式化中，如果有占位符 %s，那么按照字符串转换的协议，Python 会自动地调用相应对象的 __str__() 方法
+        # __repr__和__str__这两个方法都是用于显示的，__str__是面向用户的，而__repr__面向程序员
+        def __str__(self):
+            print('calling __str__')
+            return super(A, self).__str__()
+
+        # 如果类中没有定义__str__, 则使用__repr__，实际上__str__只是覆盖了__repr__以得到更友好的用户显示
+        def __repr__(self):
+            print('calling __repr__')
+            return super(A, self).__repr__()
+
+        def __add__(self, other):
+            print('calling __add__')
+
+    class B(object):
+        def __radd__(self, other):
+            print('calling __radd__')
+
+        # 可调用对象协议， 类似函数对象，能够让类实例表现得像函数一样
+        def __call__(self, *args, **kwargs):
+            print('calling __call__:{}'.format(args[0]))
+
+    # 定义一个容器类，所需的协议
+    class C(object):
+        # 用来支持len()函数
+        def __len__(self):
+            print('calling __len__')
+
+        # 读
+        def __getitem__(self, item):
+            print('calling __getitem__')
+
+        # 写
+        def __setitem__(self, key, value):
+            print('calling __setitem__')
+
+        # 删
+        def __delitem__(self, key):
+            print('calling __delitem__')
+
+        # 迭代器
+        def __iter__(self):
+            print('calling __iter__')
+
+        # 倒序
+        def __reversed__(self):
+            print('calling __reversed__')
+
+        # 支持in 和 not in
+        def __contains__(self, item):
+            print('calling __contains__')
+
+
+    a = A()
+    b = B()
+    # 调用__str__
+    print('%s' % a)
+    print('{}'.format(a))
+
+    a + b                         # 反运算  调用A的__add__操作，如果没有，则调用B的__radd__操作
+    b(222)                        # 类B 定义了__call__,
+
