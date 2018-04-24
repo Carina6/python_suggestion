3b066364cf9efca29b15dfe88d86ce6be514cfdc
liuhonglian [刘红莲] <liuhonglian@xiaomi.com>
2018-04-24 14:24:49 +0800

    refactor

--- leetcode/algorithms.py
+++ leetcode/algorithms.py
@@ -28,16 +28,17 @@ def test_first_uniq_char():
     print(firstUniqChar2('aadadaad'))
 
 
-def test_twoSum():
+# 1. two sum
+def test_two_sum():
     # 时间复杂度为O(n^2) 5368 ms
-    def twoSum(nums, target):
+    def two_sum(nums, target):
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return i, j
 
     # 时间复杂度为O(n) 44 ms
-    def twoSum2(nums, target):
+    def two_sum2(nums, target):
         value_dict = {}
         for i in range(len(nums)):
             if target-nums[i] in value_dict.keys():
@@ -45,4 +46,6 @@ def test_twoSum():
             else:
                 value_dict[nums[i]] = i
 
-    print(twoSum2([2, 7, 11, 15], 17))
+    print(two_sum2([2, 7, 11, 15], 17))
+
+
