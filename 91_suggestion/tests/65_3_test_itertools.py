25a473302528a8f4cf828203b62b727867efd6e3
liuhonglian [刘红莲] <liuhonglian@xiaomi.com>
2018-05-17 10:47:22 +0800

    refactor

--- 91_suggestion/tests/65_3_test_itertools.py
+++ 91_suggestion/tests/65_3_test_itertools.py
@@ -7,14 +7,20 @@ from itertools import product, permutations, combinations, combinations_with_rep
 # 组合函数
 def test_itertools2():
 
+    print()
     # 计算m个序列的n次笛卡尔积
     print(list(product('ABCD', repeat=2)))
 
+    print('1===============')
     # 产生全排列
     print(list(permutations('ABCD', 2)))
 
+    print('2===============')
     # 产生无重复元素的组合
-    print(list(combinations('ABCD', 2)))
+    # print(list(combinations('ABCD', 2)))
+    print(set(combinations([-1, 0, 1, 2, -1, -4], 3)))
+    print(list(combinations([-1, 0, 1, 2, -1, -4], 3)))
 
+    print('3===============')
     # 产生有重复元素的组合
     print(list(combinations_with_replacement('ABCD', 2)))
